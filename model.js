2798dd385aa791faac9b97fd4cd068d82044a318
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-26 11:42:11 +0000

    up ejs

--- model.js
+++ model.js
@@ -5,7 +5,7 @@ const createDomPurify = require('dompurify');
 const {JSDOM} = require('jsdom');
 const dompurify = createDomPurify(new JSDOM().window)
 
-const postSchema = new mongoose.Scheme({
+const postSchema = new mongoose.Schema({
 	title: {
 		type: String,
 		required: true
